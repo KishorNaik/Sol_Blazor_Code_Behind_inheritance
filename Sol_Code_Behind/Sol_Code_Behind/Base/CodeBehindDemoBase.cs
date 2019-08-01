3382eac8bd4e434c55468b297e772e7614596814
Kishor Naik <kishor.naik011.net@gmail.com>
2019-08-01 14:58:32 +0530

    Code Commit

---
+++ Sol_Code_Behind/Sol_Code_Behind/Base/CodeBehindDemoBase.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sol_Code_Behind.Base
+{
+    public class CodeBehindDemoBase : ComponentBase
+    {
+        #region Property
+        public String Message { get; set; } = "Hello Blazor";
+        #endregion
+
+        #region Protected Method
+        protected void FromCodeBeind()
+        {
+            Message = "Hello from Code Behind";
+        }
+        #endregion 
+    }
+}
